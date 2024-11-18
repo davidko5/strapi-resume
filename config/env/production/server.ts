9ca3d620b095de04c518310a46e071d5402e1f23
Davyd Kondratenko <dajan526@gmail.com>
2024-11-19 00:53:23 +0100

    fix: prod server.ts config

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env('HOST_URL'), // Sets the public URL of the application.
+  // url: env('HOST_URL'), // Sets the public URL of the application.
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: env.array('APP_KEYS'),
   },
 });
